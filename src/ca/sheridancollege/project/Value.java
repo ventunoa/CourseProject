4863e948b667a9ebacde6ef3e3d2039726b2f5da
xMerkery <mw3.noah@gmail.com>
2022-04-15 21:15:41 -0400

    Final commit, contains test cases and comments

---
+++ src/ca/sheridancollege/project/Value.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author Noah
+ */
+// Card values
+        public enum Value {
+             TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
+        }
